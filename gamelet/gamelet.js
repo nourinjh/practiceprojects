76af63210381ece03dba208114bac0037316720e
Nourin <nourin.abdelhadi@gmail.com>
2024-09-13 02:15:49 -0400

    formatted

--- gamelet/gamelet.js
+++ gamelet/gamelet.js
@@ -4,33 +4,33 @@
  Version: 1.0
 **************************/
 
-const ball = document.getElementById('ball'); //get the ball
-document.addEventListener('keydown', handleKeyPress);
+const ball = document.getElementById("ball"); //get the ball
+document.addEventListener("keydown", handleKeyPress);
 let positionX = 0;
 let positionY = 0;
 
-function handleKeyPress(e){
-    if(e.code == 'KeyA'){
-        positionX = positionX - 20;
-    }
-    if(e.code == 'KeyD'){
-        positionX = positionX + 20;
-    }
-    if(e.code == 'KeyW'){
-        positionY = positionY - 20;
-    }
-    if(e.code == 'KeyS'){
-        positionY = positionY + 20;
-    }
-    if(positionX < 0){
-        positionX = 0;
-        positionY = 0;
-    }
+function handleKeyPress(e) {
+  if (e.code == "KeyA") {
+    positionX = positionX - 20;
+  }
+  if (e.code == "KeyD") {
+    positionX = positionX + 20;
+  }
+  if (e.code == "KeyW") {
+    positionY = positionY - 20;
+  }
+  if (e.code == "KeyS") {
+    positionY = positionY + 20;
+  }
+  if (positionX < 0) {
+    positionX = 0;
+    positionY = 0;
+  }
 
-    refresh(); //update the ball's position
+  refresh(); //update the ball's position
 }
 
-function refresh(){ 
-    ball.style.left = positionX + 'px';
-    ball.style.top = positionY + 'px';
+function refresh() {
+  ball.style.left = positionX + "px";
+  ball.style.top = positionY + "px";
 }
